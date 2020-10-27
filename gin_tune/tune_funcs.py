5909d5edc6848b9a99ed7630bfe200777ba7f788
Sergei Volodin <sergei.volodin@epfl.ch>
2020-10-27 02:03:30 +0100

    Raise error on failure

--- gin_tune/tune_funcs.py
+++ gin_tune/tune_funcs.py
@@ -1,10 +1,13 @@
 import ray.tune as tune
 import gin
 
+# function argument the value of which the function returns
 OVERRIDE_ATTR = '_override'
 
 # map name -> function
 FUNCS = {}
+
+DEFAULT_OVERRIDE = "Use tune_gin() instead of tune.run()"
 
 def register_func(f):
     """Register tune function."""
@@ -13,15 +16,35 @@ def register_func(f):
 
 @gin.configurable
 @register_func
-def grid_search(values, _override=None):
+def grid_search(values, _override=DEFAULT_OVERRIDE):
+    if _override == DEFAULT_OVERRIDE:
+        raise ValueError(DEFAULT_OVERRIDE)
     return _override
 
 @gin.configurable
 @register_func
-def choice(categories, _override=None):
+def choice(categories, _override=DEFAULT_OVERRIDE):
+    if _override == DEFAULT_OVERRIDE:
+        raise ValueError(DEFAULT_OVERRIDE)
     return _override
 
 @gin.configurable
 @register_func
-def sample_from(func, _override=None):
+def sample_from(func, _override=DEFAULT_OVERRIDE):
+    if _override == DEFAULT_OVERRIDE:
+        raise ValueError(DEFAULT_OVERRIDE)
+    return _override
+
+@gin.configurable
+@register_func
+def uniform(lower, upper, _override=DEFAULT_OVERRIDE):
+    if _override == DEFAULT_OVERRIDE:
+        raise ValueError(DEFAULT_OVERRIDE)
+    return _override
+
+@gin.configurable
+@register_func
+def loguniform(lower, upper, base=10, _override=DEFAULT_OVERRIDE):
+    if _override == DEFAULT_OVERRIDE:
+        raise ValueError(DEFAULT_OVERRIDE)
     return _override
